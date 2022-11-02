ed3d6e0826effd7f8dc3f627da26b275bf5a5720
darylsimene <john.simene@supportzebra.com>
2022-11-02 08:58:58 +0800

    deployment files

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const user = require('./routes/user')
 const connectDB = require('./config/db') //to connect to the db
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
+const cors = require('cors')    
+const mongoSanitize = require('express-mongo-sanitize')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const hpp = require('hpp')
+const xss = require('xss-clean')
 
 dotenv.config({ path: './config/config.env' })
 
@@ -26,6 +32,18 @@ app.use(bodyParser.json()) //related to body(s) in json
 app.use(cookieParser());
 
 app.use(fileUpload());
+
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+app.use(helmet())
+app.use(cors())
+
+const limiter = rateLimit({
+    windowsMs: 10 * 60 * 1000
+})
+
+app.use(limiter)
 
 app.use(logger)
 
